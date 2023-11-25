ad4555c8f4018f8dc073d73b112079e87d2ae143
karamsafarli <karamslee007@gmail.com>
2023-11-26 01:18:21 +0400

    afn,afn

--- actions.js
+++ actions.js
@@ -2,18 +2,31 @@ const puppeteer = require('puppeteer');
 require("dotenv").config();
 
 const scrapeData = async (username, password) => {
-    const browser = await puppeteer.launch({
-        args: [
-            "--disable-setuid-sandbox",
-            "--no-sandbox",
-            "--single-process",
-            "--no-zygote",
-        ],
-        executablePath:
-            process.env.NODE_ENV === "production"
-                ? process.env.PUPPETEER_EXECUTABLE_PATH
-                : puppeteer.executablePath(),
-    });
+
+
+    let chrome = {};
+    let puppeteer;
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        chrome = require("chrome-aws-lambda");
+        puppeteer = require("puppeteer-core");
+    } else {
+        puppeteer = require("puppeteer");
+    }
+
+    let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+            args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    }
+
+    const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
     await page.goto("http://lms.adnsu.az/adnsuEducation/login.jsp");
