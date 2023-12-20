37ba314dd7e310c550bda66f37e2e20e3c59c77d
karamsafarli <karamslee007@gmail.com>
2023-12-20 16:09:48 +0400

    increased json limit

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({limit: '100mb'}));
 app.use(cors());
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '100mb', parameterLimit: 100000 }));
-// app.use(compression());
+// app.use(bodyParser.json({ limit: '100mb' }));
+// app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
+// app.use(express.raw({ limit: '50mb' }));
 
 
 
